782465edc2432ef2560ba459abcb561c48460d9b
cetinarslan-it <104364516+cetinarslan-it@users.noreply.github.com>
2022-07-19 10:37:29 +0200

    Added Student Model

--- StudentPortal.API/Program.cs
+++ StudentPortal.API/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<StudentContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentContext") ?? throw new InvalidOperationException("Connection string 'StudentContext' not found.")));
 
 // Add services to the container.
 

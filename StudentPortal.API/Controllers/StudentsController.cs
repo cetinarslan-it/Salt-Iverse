55f3f36c33672f655c974aa079454b61c7f861cb
Rinat <46563309+RinKunn@users.noreply.github.com>
2022-07-25 09:54:58 +0200

    Added list of documents to Account

--- StudentPortal.API/Controllers/StudentsController.cs
+++ StudentPortal.API/Controllers/StudentsController.cs
@@ -5,28 +5,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace StudentPortal.Api.Controllers
 {
-  [ApiController]
-  [Route("[controller]")]
-  [Authorize("shouldexists")]
-  public class StudentsController : ControllerBase
-  {
-    private readonly StudentContext _context;
-    public StudentsController(StudentContext context)
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize("shouldexists")]
+    public class StudentsController : ControllerBase
     {
-      _context = context;
-    }
+        private readonly StudentContext _context;
+        public StudentsController(StudentContext context)
+        {
+            _context = context;
+        }
 
-    [HttpGet("me")]
-    public async Task<IActionResult> UserMe()
-    {
-      var email = GetUserEmail();
-      return _context.Students != null ?
-              Ok(await _context.Students.FirstOrDefaultAsync(s => s.Email == email)) :
-              Problem("Entity is null.");
-    }
+        [HttpGet("me")]
+        public async Task<IActionResult> UserMe()
+        {
+            var email = GetUserEmail();
+            return _context.Students != null ?
+                    Ok(await _context.Students.Include(d => d.Documents)
+                    .FirstOrDefaultAsync(s => s.Email == email)) :
+            Problem("Entity is null.");
+        }
 
-    private string GetUserEmail() => 
-        User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email).Value;
-  }
+        private string GetUserEmail() =>
+            User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email).Value;
+    }
 }
 

0266ab08d9d566685cf4c81ba39d9e2613ab8be6
Rinat <46563309+RinKunn@users.noreply.github.com>
2022-07-26 14:40:38 +0200

    Added edit btn

--- studentportal-client/src/Components/AccountDetails.js
+++ studentportal-client/src/Components/AccountDetails.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
+
 
 const AccountDetails = ({ user }) => {
+    const [liEditable, setLiEditable] = useState(false);
+    const onEditClickHandled = () => {
+        setLiEditable(true);
+    };
+
     return (
         <Box
             component="form"
             sx={{
                 display: 'flex',
-                alignItems: 'flex-start',
+                alignItems: 'strech',
                 flexDirection: 'column',
-                '& > :not(style)': { m: 2, width: '35ch' },
+                '& > :not(style)': { m: 2 },
             }}
             noValidate
             autoComplete="off"
@@ -47,13 +58,24 @@ const AccountDetails = ({ user }) => {
                     readOnly: true,
                 }}
             />
-            <TextField
-                label="LinkedIn"
-                value={user.linkedInUrl}
-                InputProps={{
-                    readOnly: false,
-                }}
-            />
+
+            <Stack direction="row" spacing={2}>
+                <TextField
+                    label="LinkedIn"
+                    value={user.linkedInUrl}
+                    InputProps={{
+                        readOnly: liEditable,
+                    }}
+                    sx={{flexGrow: "1"}}
+                />
+                <IconButton variant="outlined" onClick={onEditClickHandled}>
+                    <EditIcon />
+                </IconButton>
+                <IconButton variant="outlined" disabled>
+                    <SaveIcon />
+                </IconButton>
+            </Stack>    
+
             <TextField
                 label="GitHub"
                 value={user.gitHubUrl}

4e65b6189f72f68d0a71a865c036590f4988a195
Rinat <46563309+RinKunn@users.noreply.github.com>
2022-07-27 09:45:30 +0200

    Added avatar button navbar

--- studentportal-client/src/Components/Navbar.js
+++ studentportal-client/src/Components/Navbar.js
@@ -10,9 +10,13 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
+import LogoutIcon from '@mui/icons-material/Logout';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import Tooltip from '@mui/material/Tooltip';
 import Stack from '@mui/material/Stack';
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from './LogoutButton';
@@ -24,59 +28,66 @@ import BookIcon from '@mui/icons-material/Book';
 import ScienceIcon from '@mui/icons-material/Science';
 import QuizIcon from '@mui/icons-material/Quiz';
 
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const drawerWidth = 240;
 const menuItems = [
-  { name: 'Account', icon: <PersonIcon/> },
-  { name: 'Topics', icon: <BookIcon/>},
-  { name: 'Labs', icon: <ScienceIcon/>},
-  { name: 'Assignments', icon: <QuizIcon/>}
+  { name: 'Account', icon: <PersonIcon /> },
+  { name: 'Topics', icon: <BookIcon /> },
+  { name: 'Labs', icon: <ScienceIcon /> },
+  { name: 'Assignments', icon: <QuizIcon /> }
 ];
 
 const Navbar = (props) => {
   const { window } = props;
-  const { user } = useAuth0();
+  const { user, logout } = useAuth0();
 
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   const drawer = (
     <div>
       <Toolbar
-        sx={{ color: "white", backgroundColor: "rgb(25, 118, 210)", justifyContent:"center" }}>
+        sx={{ color: "white", backgroundColor: "rgb(25, 118, 210)", justifyContent: "center" }}>
         <img src="https://precourse.salt.study/_next/static/media/salt-logo-white.3c845df7.svg" alt="salt" width="100px" />
       </Toolbar>
       <Divider />
       <List>
         {menuItems.map((menuItem, index) => (
           <ListItem key={menuItem.name} disablePadding>
-          <Button
-            sx={(theme) => ({
-              padding: '0',
-              flexGrow: '1',
-              '&.active': {
-                backgroundColor: theme.palette.primary.main,
-                color: 'white',
-              }
-            })}
-            component={NavLink}
-            to={`/${menuItem.name.toLowerCase()}`}>
-            <ListItemButton>
-              {menuItem.icon}
-              <ListItemText primary={menuItem.name} 
-                sx={{marginLeft:"20px"}}
-              />
-            </ListItemButton>
-          </Button>
-        </ListItem>
+            <Button
+              sx={(theme) => ({
+                padding: '0',
+                flexGrow: '1',
+                '&.active': {
+                  backgroundColor: theme.palette.primary.main,
+                  color: 'white',
+                }
+              })}
+              component={NavLink}
+              to={`/${menuItem.name.toLowerCase()}`}>
+              <ListItemButton>
+                {menuItem.icon}
+                <ListItemText primary={menuItem.name}
+                  sx={{ marginLeft: "20px" }}
+                />
+              </ListItemButton>
+            </Button>
+          </ListItem>
         ))}
       </List>
       <Divider />
-    
+
     </div>
   );
 
@@ -117,13 +128,50 @@ const Navbar = (props) => {
                 Student Portal
               </Typography>
             </Stack>
-            <Stack
+            {/* <Stack
               direction="row"
               justifyContent="space-between"
               alignItems="center">
+
               <LogoutButton />
+
               <Avatar alt={user.name} src={user.picture} />
-            </Stack>
+            </Stack> */}
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt={user.name} src={user.picture} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px'}}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem key="profileItem" onClick={handleCloseUserMenu} sx={{padding: 0}}>
+                  <Button sx={{ px: 5, flexGrow: 1 }} component={Link} to="/account" startIcon={<PersonIcon/>}>
+                      <Typography>Account</Typography>
+                  </Button>
+                </MenuItem>
+                <Divider/>
+                <MenuItem key="logoutItem" onClick={logout} sx={{padding: 0}}>
+                  <Button sx={{ px: 5, flexGrow: 1 }} startIcon={<LogoutIcon />}>
+                      <Typography>Logout</Typography>
+                  </Button>
+                </MenuItem>
+              </Menu>
+            </Box>
           </Stack>
         </Toolbar>
       </AppBar>
